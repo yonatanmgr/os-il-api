559ac7f665b2d584e7ed39d1391f4d166aae4943
yonatanmgr <yonatanmgr@gmail.com>
2023-03-23 02:07:31 +0200

    oops

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET" },
         ]
       }
